3a80b6f49ae74f7c39d8a2f5097cdf746338be28
wuchang <wuchang@corp.netease.com>
2018-01-05 16:20:54 +0800

    13122

---
+++ src/main/java/ReverseLinkedListII.java
@@ -0,0 +1,53 @@
+/**
+ * Created by wuchang at 1/4/18
+ * Question 92
+ *
+ 1 2 3 4 5 6 m=2 n=4
+
+ mockHead：创建一个虚拟节点，指向链表的表头，这是为了便于在翻转的时候控制，避免空指针等问题；
+ cur：指向待翻转链表的前一个位置，这里，需要翻转2 3 4，因此cur指向1；
+ rc:指向当前正在进行翻转的链表，比如，当前准备翻转2，则rc指向2，2翻转完毕指向3，则rc指向3；
+ reverseTail:已经翻转完成的链表的尾部，这里已经翻转的链表的尾部实际上就是第一个被翻转的节点，这里是节点2。比如，先翻转2， 变成 12 3456 , 然后翻转3 ， 132 456 ， 然后翻转4  1432 56，reverseTail时钟指向2；
+
+ 翻转完毕以后，让reverseTail指向下一个没翻转的节点，即，1432 56，让2的next指向5，从而实现链表的连接
+
+ */
+
+public class ReverseLinkedListII {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+
+        ListNode mockHead = new ListNode(-1);
+        mockHead.next = head; //一个指向头结点的虚拟节点
+        ListNode cur = mockHead;
+        int dis = n-m;
+        for(;m>1;m--)
+            cur = cur.next; //遍历完毕，cur指向第m个节点的前一个节点
+
+        /*
+        reverseTail指向被翻转链表的最后一个节点
+        比如12345 ，m=2,n=4，由于翻转以后节点2总是在末尾，我们让reverseTail指向2，当所有翻转结束，让reverseTail指向5,从而把链表连接起来
+         */
+        ListNode reverseTail = cur.next;
+        ListNode rc = cur.next;//rc指向当前被翻转的节点
+        cur.next = null;
+        for(;dis>=0&& rc!=null;dis--){
+             ListNode next = rc.next;//先保存下一个节点
+             rc.next = cur.next; //将当前待翻转的节点rc的next节点指向当前已经翻转的链表，从而实现待翻转节点插入到以翻转链表的前面
+             cur.next = rc;//cur指向当前被翻转链表的头结点
+             rc = next;//rc指向下一个待翻转的节点
+        }
+        reverseTail.next = rc;//将被翻转链表的尾部指向下一个节点，比如，12345，m=2,n=4，翻转以后变成1432 5，我们让2指向5，从而实现整个链表的最终连接
+        return mockHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
+        ListNode a3 = new ListNode(3);
+        ListNode a4 = new ListNode(4);
+        a1.next = a2;
+        a2.next = a3;
+        a3.next = a4;
+        new ReverseLinkedListII().reverseBetween(a1,1,5);
+    }
+}
--- src/main/java/UniqueBinarySearchTrees.java
+++ src/main/java/UniqueBinarySearchTrees.java
@@ -3,6 +3,13 @@ import java.util.Map;
 
 /**
  * Question 96
+ * [Unique Binary Search Trees](https://leetcode.com/problems/unique-binary-search-trees/description/)
+ [Unique Binary Search Tree II](https://leetcode.com/problems/unique-binary-search-trees-ii/description/)
+ [Different Ways to Add Parentheses](https://leetcode.com/problems/different-ways-to-add-parentheses/description/)这三道题的思想是一致的。
+
+ 使用动态规划的方式非常明显。具有最优字结构，即对于n，选出一个root节点，有n中选择方式，则n的BST数量是这n中选择形成的BST的数量之和，即具备DP的最优字结构的特点。而bottom case是n=0和n=1;
+
+ 是否可以尝试使用自底向上的方式，即，给出一个n，从i=0开始往上计算，一直计算到n呢？
  */
 
 public class UniqueBinarySearchTrees {
